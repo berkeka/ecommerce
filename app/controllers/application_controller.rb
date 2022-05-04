9e3c6e261c71ea48627b41fa0965268c30818492
Berke Kalkan <berkekalkantr@gmail.com>
2022-05-05 00:55:33 +0300

    Create categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :fetch_categories
+
+  protected
+
+  def fetch_categories
+    @categories = Category.includes(:sub_categories).all
+  end
 end
