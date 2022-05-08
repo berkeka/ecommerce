8c3c9b72e0c8fa93d2c48d214e6b4c5b677baf20
Berke Kalkan <berkekalkantr@gmail.com>
2022-05-08 07:48:56 +0300

    Create comparison logic and UIs

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -4,17 +4,19 @@ class CompareController < ApplicationController
   before_action :set_first_product
   before_action :set_second_product, only: :show
 
-  def index; end
+  def index
+    @products = Product.where(sub_category: @first_product.sub_category)
+  end
 
   def show; end
 
   private
 
   def set_first_product
-    @first_product = Product.find(params[:product_id])
+    @first_product = Product.includes(:category, :sub_category).find(params[:product_id])
   end
 
   def set_second_product
-    @first_product = Product.find(params[:id])
+    @second_product = Product.find(params[:id])
   end
 end
